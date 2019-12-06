c4ab2c3325f765486618892364792ae21f57cd3e
collinkallery <collin.kallery@colorado.edu>
2019-12-06 11:51:39 -0700

    Push for emoji challenge

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+var happyButton = document.querySelector(".happy");
+var sillyButton = document.querySelector(".silly");
+var sadButton = document.querySelector("crying");
+var inputField = document.querySelector(".message");
 
+var sillyQuotes = ["Interesting response.", "Sounds ...good?", "Me too."]
+var sadQuotes = ["Do you want to talk?", "Keep your head up!", "We can cry together"]
+
+function happyResponses() {
+  // When a user clicks the happy emoji, I want it to cycle through the happyQuotes array at random.
+  var happyQuotes = ["You go!", "Your smile just cheered me up!", "YAY!"];
+  inputField.innerText = happyQuotes[0];
+}
+
+happyButton.addEventListner("click", happyResponses);
+// sillyButton.addEventListner("click", sillyResponses);
+// sadButton.addEventListner("click", sadResponses);
